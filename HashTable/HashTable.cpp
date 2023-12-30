26ade1c55b74eea1e0891c506e93b2a2b918d03a
LSH <dltpgns97@gmail.com>
2023-12-30 14:50:57 +0900

    hash table auto 제거, delete 부분 수정

--- HashTable/HashTable.cpp
+++ HashTable/HashTable.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <random>
 
 using namespace std;
 
@@ -22,8 +23,8 @@ public:
             data_[hash] = new vector<pair<int, int>*>;
         }
 
-        auto& hash_row = *data_[hash];
-        for(const auto& hash_element : hash_row)
+        vector<pair<int, int>*> &hash_row = *data_[hash];
+        for(pair<int, int>* &hash_element : hash_row)
         {
             if(hash_element->first == key)
             {
@@ -31,16 +32,15 @@ public:
                 return;
             }
         }
-
-        hash_row.push_back(new pair<int,int>(key, value));
+        data_[hash]->push_back(new pair<int, int>(key, value));
     }
 
     int get_element(const int key) const
     {
         const int hash = hash_function(key);
         if(data_[hash] == nullptr) return -999;
-        const auto& hash_row = *data_[hash];
-        for(auto& hash_element : hash_row)
+        vector<pair<int, int>*> &hash_row = *data_[hash];
+        for(pair<int, int>* &hash_element : hash_row)
         {
             if(hash_element->first == key)
             {
@@ -50,36 +50,55 @@ public:
         return 0;
     }
 
-    void remove_element(const int key) const
+    void remove_element(const int key)
     {
         const int hash = hash_function(key);
-        
-        auto& hash_row = *data_[hash];
-        for(const auto& hash_element : hash_row)
-        {
-            int index = 0;
-            if(hash_element->first == key)
+        if (data_[hash] == nullptr) return;
+
+        vector<pair<int, int>*> &hash_row = *data_[hash];
+        for (int i = 0; i < hash_row.size(); i++) {
+            if (hash_row[i]->first == key)
             {
-                delete hash_element;
-                hash_row.erase(hash_row.begin() + index);
-                return;
+                delete hash_row[i];
+                hash_row[i] = nullptr;
+                hash_row.erase(hash_row.begin() + i);
+                break;
             }
-            index++;
+        }
+
+        if (data_[hash]->empty()) {
+            delete data_[hash];
+            data_[hash] = nullptr;
         }
     }
 
+    vector<int> get_keys() {
+        vector<int> keys;
+        for (int i = 0; i < data_.size(); i++)
+        {
+            if (data_[i] == nullptr)
+            {
+                continue;
+            }
+            vector<pair<int, int>*> &hash_row = *data_[i];
+            for (pair<int, int>* &hash_element : hash_row)
+            {
+                keys.push_back(hash_element->first);
+            }
+        }
+        return keys;
+    }
     void print_table() const
     {
         for(int i = 0; i < data_.size(); i++)
         {
-            cout << i <<"\n";
             if(data_[i] == nullptr)
             {
-                cout << "----------------------------------" <<"\n";
                 continue;
             }
-            const auto hash_row = *data_[i];
-            for(auto& hash_element : hash_row)
+            cout << i << "\n";
+            vector<pair<int, int>*> &hash_row = *data_[i];
+            for(pair<int, int>* &hash_element : hash_row)
             {
                 cout << "key : "<< hash_element->first << " / value : "<< hash_element->second << "\n";
             }
@@ -97,17 +116,21 @@ private:
 // https://www.cs.usfca.edu/~galles/visualization/OpenHash.html Âü°í
 int main(int argc, char* argv[])
 {
-    hash_table table(3);
- 
-    table.insert_element(0,0);
-    table.insert_element(6,8);
-    table.insert_element(6,7);
-    table.insert_element(1,1);
-    table.insert_element(2,2);
-    table.insert_element(12,21);
-    table.remove_element(0);
+    hash_table table(100);
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution<int> dist(0, 500);
     
-    table.print_table();
+   while(1) {
+        const int key = dist(mt);
+        const int value = dist(mt);
+        const int remove_key = dist(mt);
+        table.insert_element(key, value);
+        table.remove_element(remove_key);
+        table.print_table();
+    }
+    
+    
 
     return 0;
 }
